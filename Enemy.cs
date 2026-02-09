6a7dc4f827f52e5f171a930b1b5e14db3f0c1085
wingsmove <wu.5856@buckeyemail.osu.edu>
2026-02-09 17:30:16 -0500

    Added Enemy sprite class, similar to player, should be an animated moving sprite Added MapBlock sprite class, should be an non animated non moving sprite

---
+++ Enemy.cs
@@ -0,0 +1,14 @@
+using System.Numerics;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Enemy  : ISprite
+{
+    public void Update()
+    {
+
+    }
+    public void Draw(SpriteBatch spriteBatch, Vector2 startCoords)
+    {
+
+    }
+}
