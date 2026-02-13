3b419d2b9ad50169965a3b9e97f4fcf72678f03f
cyanchicken1 <lohith.katari@gmail.com>
2026-02-13 13:10:36 -0500

    Icommand and Key/Mouse Controllers
    added code for

---
+++ Commands/QuiteCommand.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+
+public class QuitCommand : ICommand
+{
+    private readonly Game game;
+
+    public QuitCommand(Game game)
+    {
+        this.game = game;
+    }
+
+    public void Execute()
+    {
+        game.Exit();
+    }
+}
