fa1d537b2d206f33e2f18239549e8546a387ddd9
cyanchicken1 <lohith.katari@gmail.com>
2026-02-15 21:03:58 -0500

    command pattern player

---
+++ Commands/Player/JumpPressedCommand.cs
@@ -0,0 +1,19 @@
+﻿public sealed class JumpPressedCommand : ICommand
+{
+    // Placeholder for later:
+    // private readonly Player _player;
+
+    public JumpPressedCommand()
+    {
+        // Later: accept Player in constructor
+        // public JumpPressedCommand(Player player) => _player = player;
+    }
+
+    public void Execute()
+    {
+        // Later:
+        // _player.JumpPressed();
+
+
+    }
+}
