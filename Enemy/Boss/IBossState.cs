37f3a66911f311e69880c43d21e14ae9a6154c6c
Ravi Wijeratne <raviwijeratne@Ravis-MacBook-Air.local>
2026-04-22 13:27:45 -0400

    adds idle state for the boss and a placeholder hitbox

---
+++ Enemy/Boss/IBossState.cs
@@ -0,0 +1,10 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public interface IBossState
+{
+    void OnEnter(Boss boss);
+    void Update(Boss boss, GameTime gameTime);
+    void Draw(Boss boss, SpriteBatch spriteBatch);
+
+}
---
+++ Enemy/Boss/boss.cs
@@ -0,0 +1,90 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+public class Boss : ISprite, IEnemy
+{
+    private Texture2D texture;
+    private Vector2 position;
+    private bool isDead;
+    private int currentFrame;
+    private Vector2 velocity;
+    private bool facingLeft;
+
+    private IBossState currentState;
+
+    public bool IsDead => isDead;
+    public Rectangle sourceRectangle;
+
+    public Boss(Texture2D texture, Vector2 startPos)
+    {
+        this.texture = texture;
+        this.position = startPos;
+        this.isDead = false;
+        this.currentFrame = 0;
+        this.facingLeft = true;
+        currentState = new BossIdleState();
+        currentState.OnEnter(this);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        this.velocity = new Vector2(50, 0);
+        currentState.Update(this, gameTime);
+
+    }
+    public void Draw(SpriteBatch spriteBatch, Vector2 startPos)
+    {
+        var direction = facingLeft ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+        currentState.Draw(this, spriteBatch);
+        spriteBatch.Draw(texture, position, sourceRectangle, Color.White, 0f, Vector2.Zero, 0.5f, direction, 0f);
+    }
+    // TODO: finish these, currently placeholders
+    public Rectangle GetBounds()
+    {
+        return new Rectangle((int)position.X, (int)position.Y, 56, 64);
+    }
+    public void TakeDamage()
+    {
+        isDead = true;
+        velocity = Vector2.Zero;
+    }
+    public void BounceX()
+    {
+        velocity.X *= -1;
+        facingLeft = !facingLeft;
+    }
+    public void BounceY()
+    {
+        velocity.Y *= -1;
+    }
+    public float GetVelocityX() => velocity.X;
+    public float GetVelocityY() => velocity.Y;
+    public void ResolveCollisions(List<CollisionResult> results)
+    {
+
+        foreach (var result in results)
+        {
+            if (result.IsHarmful)
+            {
+                TakeDamage();
+                continue;
+            }
+
+            if (!result.IsCollideable) continue;
+
+            switch (result.Direction)
+            {
+                case CollisionDirection.Left:
+                case CollisionDirection.Right:
+                    BounceX();
+                    break;
+                case CollisionDirection.Up:
+                case CollisionDirection.Down:
+                    BounceY();
+                    break;
+            }
+        }
+    }
+}
