08d96db1c07b337e13de085a5babde5fd3e1cfb6
Ravi Wijeratne <raviwijeratne@Ravis-MacBook-Air.local>
2026-04-23 14:09:09 -0400

    adds framework for boss states and design doc

--- Enemy/Boss/BossAttackState.cs
+++ Enemy/Boss/BossAttackState.cs
@@ -1,13 +1,10 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-// TODO: finish implementation 
-// Right now is just a copy of the attack recovery state
-// Should play one invincible animation entering vulerability
-// then stay vulnerable for a certain amount of time
-public class BossVulnerableState : IBossState
+// TODO: finish implementation
+public class BossAttackState : IBossState
 {
-    private const float SecondsPerFrame = 0.15f;
+    private const float SecondsPerFrame = 0.1f;
     private const int TotalFrames = 5;
 
     private int currentFrame = 0;
@@ -15,10 +12,9 @@ public class BossVulnerableState : IBossState
     private bool commandReceivedThisFrame = false;
     private double timer = 0;
     private readonly double runDuration = 4.0; // Run for 3 seconds
-    private int frameWidth = 655;
     public void OnEnter(Boss boss)
     {
-        boss.sourceRectangle = new Rectangle(6, 4388, frameWidth, 578);
+        boss.sourceRectangle = new Rectangle(3, 1256, 623, 490);
         commandReceivedThisFrame = false;
         timer = 0;
     }
@@ -27,13 +23,15 @@ public class BossVulnerableState : IBossState
     {
         AdvanceFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
         // Update the source rectangle here
-        int gap = 6;
-        int startX = 6;
+        int frameWidth = 623;
+        int gap = 3;
+        int startX = 3;
 
         int newX = startX + (currentFrame * (frameWidth + gap));
 
         // Update the boss's source rectangle
         // Note: Ensure the height (373 vs 395) is consistent with your sprite sheet
+        boss.sourceRectangle = new Rectangle(newX, 1256, frameWidth, 490);
         timer += gameTime.ElapsedGameTime.TotalSeconds;
         if (timer >= runDuration)
         {
